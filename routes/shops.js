1345c6d2b8dcd6add49f3f9b2e8d92eca45405f8
RainyCityDiver <cvmembership@posteo.net>
2022-08-15 18:02:00 -0700

    Removed findshops.js & added boilerplate for GET by queries and POST.

--- routes/shops.js
+++ routes/shops.js
@@ -2,11 +2,60 @@ const { Router } = require('express');
 const router = Router();
 const shopData = require('../dataInterface/shops');
 
-// Route to retrieve (GET) all movies from database
-// curl http://localhost:5000/movies
+// Route to retrieve (GET) all shops from database
+// curl http://localhost:5000/shops
 router.get('/', async (req, res) => {
   let resultStatus;
   const result = await shopData.getAllShops();
+
+  if (result === null) {
+    resultStatus = 500;
+  } else if (result.length === 0) {
+    resultStatus = 404;
+  } else if (result.error) {
+    resultStatus = 400;
+  } else {
+    resultStatus = 200;
+  }
+
+  res.status(resultStatus).send(result);
+});
+
+// curl 'http://localhost:5000/shops?query-item=param'
+// curl 'http://localhost:5000/shops?state=ca'
+router.get('/', async (req, res) => {
+  
+  if (!req.query) {
+    res.status(404).send({ error: 'No query parameters found.' });
+  }
+  
+  let resultStatus;
+  let queryObj = {};
+
+  if (req.query.state) {
+    state = req.query.state;
+    queryObj['state'] = state;
+  }
+
+  let result = await shopData.getByParameter(queryObj);
+
+  if (result === null) {
+    resultStatus = 500;
+  } else if (result.length === 0) {
+    resultStatus = 404;
+  } else if (result.error) {
+    resultStatus = 400;
+  } else {
+    resultStatus = 200;
+  }
+
+  res.status(resultStatus).send(result);
+});
+
+// curl -X POST -H "Content-Type: application/json" -d '{"website":"http://website.com", "name":"test-shop", "state":"WA", "address":"1234 Main St. Seattle WA", "bikes":"true", "tools":"true"}' http://localhost:5000/shops
+router.post('/', async (req, res) => {
+  let resultStatus;
+  let result = await shopData.createShopDocument(req.body);
 
   if (result === null) {
     resultStatus = 500;
