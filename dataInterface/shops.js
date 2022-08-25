7fbd99b8c8cc521c8be47f7c828b54eaabd3fbf8
RainyCityDiver <cvmembership@posteo.net>
2022-08-25 15:02:57 -0700

    Upload so team can examine.

--- dataInterface/shops.js
+++ dataInterface/shops.js
@@ -1,9 +1,7 @@
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
-// uri is from mongodb account > Connect > Connect your app > Driver: Node
-const uri = process.env.MONGODB_URI;
-
+const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
 
 const databaseName = 'free-bike-finder';
@@ -14,17 +12,30 @@ const shopData = database.collection(collName);
 
 module.exports = {};
 
+findCompanyByName = async (name) => {
+  const query = {name: name};
+  let company = await shopData.findOne(query);
+  return company;
+};
+
+findCompanybyPhone = async (phone) => {
+  const query = {phone: phone};
+  let company = await shopData.findOne(query);
+  return company;
+}
+
 // https://www.mongodb.com/docs/drivers/node/current/usage-examples/find/
 module.exports.getAllShops = async () => {
+  const database = client.db(databaseName);
+  const shopData = database.collection(collName);
+  
   const query = {};
-  let cursor = await shopData.find(query);
-  console.log('test');
+  let cursor = await shopData.find(query).limit(10);
+  console.log(query);
   return cursor.toArray();
 };
 
 module.exports.getByParameter = async (queryObj) => {
-  const database = client.db(databaseName);
-  const shopData = database.collection(collName);
   let result = await shopData.find(queryObj);
   return cursor
     ? cursor.toArray()
@@ -34,10 +45,32 @@ module.exports.getByParameter = async (queryObj) => {
 };
 
 module.exports.createShopDocument = async (itemsToInsert) => {
-  const database = client.db(databaseName);
-  const shopData = database.collection(collName);
   const query = { ...itemsToInsert };
   let result = await shopData.insertOne(query);
 
   return result;
+};
+
+module.exports.updateById = async (shopId, newObj) => {
+  // Stealing the following filter because it makes the updateOne() code easier to read.
+  const updateRules = {
+    $set: {"website" : newObj.website, "name": newObj.name, "address": newObj.address, "phone": newObj.phone, "email": newObj.email, "contact_form": newObj.contact_form, "facebook": newObj.facebook, "twitter": newObj.twitter, "instagram": newObj.instagram, "cost": newObj.cost, "helmets": newObj.bikes, "maintenance": newObj.maintenance, "tools": newObj.tools, "classes": newObj.classes, "suggested_donation": newObj.suggested_donation, "volunteering": newObj.volunteering, "donate_bikes": newObj.donate_bikes, "target_client": newObj.target_client, }
+  };
+  const filter = { _id: ObjectId(shopId) };
+  const result = await shopData .updateOne(filter, updateRules);
+
+  if(result.modifiedCount != 1){
+    return {error: `Something went wrong. ${result.modifiedCount} movies were updated. Please try again.`}
+  };
+};
+
+module.exports.deleteById = async (movieId) => {
+  const deletionRules = {_id:ObjectId(movieId)}
+  const result = await shopData.deleteOne(deletionRules);
+
+  if(result.deletedCount != 1){
+    return {error: `Something went wrong. ${result.deletedCount} movies were deleted. Please try again.`}
+  };
+
+  return {message: `Deleted ${result.deletedCount} movie.`};
 };
--- routes/shops.js
+++ routes/shops.js
@@ -21,8 +21,12 @@ router.get('/', async (req, res) => {
   res.status(resultStatus).send(result);
 });
 
+// Route to retrieve shops by query parameters: state, maintenance, classes
 // curl 'http://localhost:5000/shops?query-item=param'
 // curl 'http://localhost:5000/shops?state=ca'
+// curl 'http://localhost:5000/shops?maintenance=TRUE'
+// curl 'http://localhost:5000/shops?classes=TRUE'
+// curl 'http://localhost:5000/shops?state=ca&maintenance=TRUE&classes=FALSE'
 router.get('/', async (req, res) => {
   
   if (!req.query) {
@@ -52,7 +56,7 @@ router.get('/', async (req, res) => {
   res.status(resultStatus).send(result);
 });
 
-// curl -X POST -H "Content-Type: application/json" -d '{"website":"http://website.com", "name":"test-shop", "state":"WA", "address":"1234 Main St. Seattle WA", "bikes":"true", "tools":"true"}' http://localhost:5000/shops
+// curl -X POST -H "Content-Type: application/json" -d '{"website":"http://website.com", "name":"test-shop", "state":"TS", "address":"1234 Main St. City TS", "bikes":"true", "tools":"true" "phone":"(123) 456-7890"}' http://localhost:5000/companies
 router.post('/', async (req, res) => {
   let resultStatus;
   let result = await shopData.createShopDocument(req.body);
@@ -62,6 +66,47 @@ router.post('/', async (req, res) => {
   } else if (result.length === 0) {
     resultStatus = 404;
   } else if (result.error) {
+    resultStatus = 400;
+  } else {
+    resultStatus = 200;
+  }
+
+  res.status(resultStatus).send(result);
+});
+
+// curl -X PUT -H "Content-Type: application/json" -d '{"bikes":"TRUE"}' http://localhost:5000/companies/test-shop
+router.put("/:name", async (req, res, next) => {
+  let resultStatus;
+  const result = await movieData.updateByName(req.params.name, req.body)
+
+  if(result.error){
+    resultStatus = 400;
+  } else {
+    resultStatus = 200;
+  }
+
+  res.status(resultStatus).send(result);
+});
+
+// curl -X PUT -H "Content-Type: application/json" -d '{"bikes":"TRUE"}' http://localhost:5000/companies/(123) 456-7890
+router.put("/:phone", async (req, res, next) => {
+  let resultStatus;
+  const result = await movieData.updateByPhone(req.params.phone, req.body)
+
+  if(result.error){
+    resultStatus = 400;
+  } else {
+    resultStatus = 200;
+  }
+
+  res.status(resultStatus).send(result);
+});
+
+// curl -X DELETE http://localhost:5000/movies/573a1390f29313caabcd4135
+router.delete("/:id", async (req, res, next) => {
+  const result = await movieData.delete(req.params.id);
+
+  if(result.error){
     resultStatus = 400;
   } else {
     resultStatus = 200;
