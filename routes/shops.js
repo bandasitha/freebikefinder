64f7777d62e1f7867fc680a89536e0b91be4b28e
RainyCityDiver <cvmembership@posteo.net>
2022-08-27 16:59:53 -0700

    Get routes working, may need finalization but maybe not.

--- routes/shops.js
+++ routes/shops.js
@@ -3,11 +3,18 @@ const router = Router();
 const shopData = require('../dataInterface/shops');
 
 // Route to retrieve (GET) all shops from database
-// curl http://localhost:5000/shops
+// curl 'http://localhost:5000/shops'
+// curl 'http://localhost:5000/shops?state=ri'
+// works
 router.get('/', async (req, res) => {
+  let result;
   let resultStatus;
-  const result = await shopData.getAllShops();
-
+  if (!req.query) {
+    result = await shopData.getAllShops();
+  } else {
+    let state = req.query.state.toUpperCase();
+    result = await shopData.getShopByParameter(state);
+  }
   if (result === null) {
     resultStatus = 500;
   } else if (result.length === 0) {
@@ -17,43 +24,15 @@ router.get('/', async (req, res) => {
   } else {
     resultStatus = 200;
   }
-
   res.status(resultStatus).send(result);
 });
 
 // Route to retrieve (GET) one shop from database by _id value
 // curl http://localhost:5000/shops/630a74a5423ebfea5ae6acc3
+// works
 router.get('/:id', async (req, res) => {
   let resultStatus;
   const result = await shopData.getShopById(req.params.id);
-
-  if (result === null) {
-    resultStatus = 500;
-  } else if (result.length === 0 || result.error) {
-    resultStatus = 404;
-  } else {
-    resultStatus = 200;
-  }
-
-  res.status(resultStatus).send(result);
-});
-
-// curl 'http://localhost:5000/shops?query-item=param'
-// curl 'http://localhost:5000/shops?state=ca'
-router.get('/', async (req, res) => {
-  if (!req.query) {
-    res.status(404).send({ error: 'No query parameters found.' });
-  }
-
-  let resultStatus;
-  let queryObj = {};
-
-  if (req.query.state) {
-    state = req.query.state;
-    queryObj['state'] = state;
-  }
-
-  let result = await shopData.getShopByParameter(queryObj);
 
   if (result === null) {
     resultStatus = 500;
@@ -69,6 +48,7 @@ router.get('/', async (req, res) => {
 });
 
 // curl -X POST -H "Content-Type: application/json" -d '{"website":"http://test-shop.com", "name":"Test Shop", "state":"WA", "address":"1234 Main St. Seattle WA", "phone":"(234)456-5678", "email":"test@test.co"}' http://localhost:5000/shops
+// works
 router.post('/', async (req, res) => {
   let resultStatus;
   let result = await shopData.createShop(req.body);
@@ -87,6 +67,7 @@ router.post('/', async (req, res) => {
 });
 
 // curl -X PUT -H "Content-Type: application/json" -d '{"website":"http://test-shop-update.com", "name":"Test Shop", "state":"WA", "address":"1234 Main St. Seattle WA", "phone":"(234)456-5678", "email":"test@test-update.co"}' http://localhost:5000/shops/630a74a5423ebfea5ae6acc3
+// works
 router.put('/:id', async (req, res) => {
   let resultStatus;
   const result = await shopData.updateShopById(req.params.id, req.body);
@@ -103,6 +84,7 @@ router.put('/:id', async (req, res) => {
 });
 
 // curl -X DELETE http://localhost:5000/shops/<_id here>
+// doesn't work
 router.delete('/:id', async (req, res) => {
   let resultStatus;
   const result = await shopData.deleteByID(req.params.id);
