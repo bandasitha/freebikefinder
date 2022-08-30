ef87b131644a3a22e43622a2d3bccf0f8c1594f1
RainyCityCoder <42524138+RainyCityCoder@users.noreply.github.com>
2022-08-29 23:41:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const server = require('./server');
 
-const port = 8000;
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 server.listen(port, () =>
   console.log(`Server is listening on http://localhost:${port}`)
 );
