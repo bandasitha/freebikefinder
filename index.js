b5c7463f8cf3590e52293eeebdcea4c8acdf6b6b
RainyCityDiver <cvmembership@posteo.net>
2022-08-29 23:04:28 -0700

    heroku push

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const server = require('./server');
 
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-let port = 8000;
-// }
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 server.listen(port, () =>
   console.log(`Server is listening on http://localhost:${port}`)
