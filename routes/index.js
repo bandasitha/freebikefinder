d394c3fcfbe100c536f3bed6a1014ac176a679d2
RainyCityDiver <cvmembership@posteo.net>
2022-08-30 21:16:28 -0700

    heroku push

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 // require('dotenv').config();
 
-router.use('/anything', require('./shops'));
+router.use('/shops', require('./shops'));
 router.use('/', (req, res) =>
   res.status(404).send('Route not found. Maybe you meant /shops?')
 );
