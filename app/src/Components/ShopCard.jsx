d042b310413b841097012cf1968e45cefe7497e2
Admin <admin@Admins-MacBook-Pro.local>
2022-09-03 13:10:43 -0700

    Added React routes for helmets and nonprofits + both by state.

--- app/src/Components/ShopCard.jsx
+++ app/src/Components/ShopCard.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ShopCard.css';
 
-export default function ShopCard({ shopId, shopName, shopAddress, shopState }) {
+export default function ShopCard({
+  shopId,
+  shopName,
+  shopAddress,
+  shopState,
+  asset,
+}) {
   return (
     <div className='shop'>
       <h2>
         <a href={`../shop/${shopId}`}>{shopName}</a>
       </h2>
       <address>{shopAddress}</address>
-      <p>
-        State = <a href={`./shops/${shopState}`}>{shopState}</a>
-      </p>
+      {shopState ? (
+        <p>
+          <a href={`/${asset}/${shopState.toLowerCase()}`}>{shopState}</a>
+        </p>
+      ) : (
+        ''
+      )}
     </div>
   );
 }
